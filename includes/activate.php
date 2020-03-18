1b5f389f52694328816a9a15d5a6ea61a4bdead5
Nikolas Haug <nikohaug21@gmail.com>
2020-03-18 15:42:48 -0500

    add db table creation to active file

--- includes/activate.php
+++ includes/activate.php
@@ -1,7 +1,22 @@
-<?php 
+<?php
 
-function r_activate_plugin() {
-    if( version_compare( get_bloginfo('version'), '5.0', '<' ) ) {
-        wp_die( __('You must update Wordpress to use this plugin.'), 'recipe' );
+function r_activate_plugin(){
+    // 5.8 < 5.0
+    if( version_compare( get_bloginfo( 'version' ), '5.0', '<' ) ){
+        wp_die( __( "You must update WordPress to use this plugin.", 'recipe' ) );
     }
+
+    global $wpdb;
+    $createSQL      =   "
+    CREATE TABLE `" . $wpdb->prefix . "recipe_ratings` (
+        `ID` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+        `recipe_id` BIGINT(20) UNSIGNED NOT NULL,
+        `rating` FLOAT(3,2) UNSIGNED NOT NULL,
+        `user_ip` VARCHAR(50) NOT NULL,
+        PRIMARY KEY (`ID`)
+    ) ENGINE=InnoDB " . $wpdb->get_charset_collate() . ";";
+
+
+    require( ABSPATH . "/wp-admin/includes/upgrade.php" );
+    dbDelta( $createSQL );
 }
