aa2b9661280435013dfd05ee0233ba63d48bcfc6
Nikolas Haug <nikohaug21@gmail.com>
2020-03-18 14:56:43 -0500

    add save post meta folder

--- index.php
+++ index.php
@@ -18,9 +18,11 @@
  // Includes 
  include( 'includes/activate.php' ); 
  include( 'includes/init.php' );
+ include( 'process/save-post.php' );
 
  // Hooks 
 register_activation_hook( __FILE__, 'r_activate_plugin' );
 add_action( 'init', 'recipe_init' );
+add_action( 'save_post_recipe', 'r_save_post_admin', 10, 3 );
 
  // Shortcodes
